f59d759b025b6b8f85d7ba9fa36acb9cdf4b0401
Nandini Ghata <152566146+nano13579@users.noreply.github.com>
2025-11-25 12:24:13 -0800

    astro, tailwind, shadcn/ui

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {
+        fontFamily: {
+            space: ['space-mono', 'sans-serif'],
+        },
+    },
+  },
+};   
